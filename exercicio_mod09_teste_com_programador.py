4d9eb3279f30ed9038278651a86d2f17c9f4e568
luuizhgentil-create <luuizhgentil@gmail.com>
2026-04-29 17:00:56 -0300

    Correct Celular class implementation and error handling
    Fix indentation and syntax errors in the Celular class.

--- exercicio_mod09_teste_com_programador.py
+++ exercicio_mod09_teste_com_programador.py
@@ -7,4 +7,14 @@ class Celular:
             nivel=float(entrada)
             if nivel<0 or nivel>100:
                 print("Valor inválido! Digite de 0 a 100.")
-                elif nível
+                elif nível <15:
+                    print(f"Bateria em {nivel}%! Carregue já.")
+            elif nivel>85:
+        print(f"Bateria em {nivel}%. Carga exelente!")
+    else:
+        print(f"Bateria em nivel {nivel}%. Status normal.")
+    except ValueError:
+        print("Erro: Digite apenas números!")
+
+cel=Celular("Samsung", "S24")
+cel.verificar_status()
