12c73528cdd2c47bed991abdf957a62b80a2bd64
luuizhgentil-create <luuizhgentil@gmail.com>
2026-04-29 17:27:00 -0300

    Refactor Celular class and fix method definitions
    Fix indentation and syntax errors in Celular class methods.

--- exercicio_mod09_teste_com_programador.py
+++ exercicio_mod09_teste_com_programador.py
@@ -1,1 +1,16 @@
+class Celular:
+  def__init__(self, marca, modelo):
+    self.marca, self.modelo=marca, modelo
+    self.bateria=100
 
+def fazer_chamada(self, custo_bateria):
+  print(f"\n--Chamada no {self.modelo}--"}
+
+try:
+  self.bateria-= custo_bateria
+except TypeError:
+  print(f"ERRO: O custo da bateria deve ser um número!")
+else:
+  print(f"Sucesso! Bateria atual: {self.bateria}%")
+finally:
+  print("Sistema finalizado.")
